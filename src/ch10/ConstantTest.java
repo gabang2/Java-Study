fb851ef4ffbb8894129a6d0aa2a2144d7a459117
gabang2 <offbeat1020@naver.com>
2022-12-04 00:15:13 +0900

    10강 : 상수와 형변환

---
+++ src/ch10/ConstantTest.java
@@ -0,0 +1,12 @@
+package ch10;
+
+public class ConstantTest {
+	public static void main(String[] args) {
+		
+		final int MAX_NUM = 100;
+		// MAX_NUM = 200; // 에러 뜸
+		
+		System.out.print(MAX_NUM);
+		
+	}
+}
