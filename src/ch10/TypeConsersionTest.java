2eb4d14b200a0b9e44c00be42cad66115066ef14
gabang2 <offbeat1020@naver.com>
2022-12-04 14:35:40 +0900

    11강 : 연산자

--- src/ch10/TypeConsersionTest.java
+++ src/ch10/TypeConsersionTest.java
@@ -9,22 +9,24 @@ public class TypeConsersionTest {
 		int iNum = bNum;
 		System.out.println(iNum);
 		
-		// 암시적 형변환(변수 캐스팅 과정)
+		// 명시적 형변환(변수 캐스팅 과정)
 		int iNum2 = 255;
 		byte bNum2 = (byte)iNum2; // 127이상을 가질 수 없기 때문에 -1으로 저장됨
 		System.out.println(bNum2);
 		
+		// 명시적 형변환(변수 캐스팅 과정)
 		double dNum = 3.14;
 		int iNum3 = (int)dNum;
 		System.out.println(iNum3);
 		
-		// 암시적 형변환(변수 캐스팅 과정)
+		// 명시적 형변환(변수 캐스팅 과정)
 		double dNum2 = 1.2;
 		float fNum = 0.9F;
 		int iNum4 = (int)dNum2 + (int)fNum;
 		int iNum5 = (int)(dNum2 + fNum);
 		System.out.println(iNum4);
 		System.out.println(iNum5);
+		
 	}
 
 }
