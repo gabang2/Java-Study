0f4544029c10bd67c2a9de1cfd01bee881c70d72
gabang2 <offbeat1020@naver.com>
2022-12-06 20:49:58 +0900

    ch02 - 2강  : class 개념

---
+++ Chapter02/src/ch01/Student.java
@@ -0,0 +1,14 @@
+package ch01;
+
+public class Student { // public은 어디서든 접근 가능한 그런 것이다.
+	// class의 이름은 대문자로 시작하는 것이 좋다.
+	// package의 이름은 소문자로 시작하는 것이 좋다.
+	// 하나의 java 파일 중에서 class는 여러 개가 있을 수 있으나, public class는 꼭 있어야 한다.
+	
+	int studentNumber; // 접근 제어자
+	String studentName;
+	int majorCode;
+	String majorName;
+	int grade;
+	
+}
