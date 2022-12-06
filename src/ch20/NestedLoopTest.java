895f3bd14cbc52183364dad14650d024dd0af15d
gabang2 <offbeat1020@naver.com>
2022-12-06 15:12:46 +0900

    21강 : break과continue문

--- src/ch20/NestedLoopTest.java
+++ src/ch20/NestedLoopTest.java
@@ -3,12 +3,14 @@ package ch20;
 public class NestedLoopTest {
 
 	public static void main(String[] args) {
+		// for문 중첩반복문
 		for( int dan = 2; dan <=9; dan++) {
 			for (int count = 1; count<=9; count ++) {
 				System.out.println("for문-"+dan+"x"+count+"="+dan*count);
 			}
 		}
 		
+		// while문 중첩반복문
 		int dan = 2;
 		int count = 1;
 		while(dan<=9) {
