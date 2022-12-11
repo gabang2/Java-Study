dde832013a14eb922b91abb36b92fde0b32fb412
gabang2 <offbeat1020@naver.com>
2022-12-11 16:34:55 +0900

    14 : 인터페이스의 여러가지 요소

---
+++ Chapter03/src/ch14/CalculatorTest.java
@@ -0,0 +1,13 @@
+package ch14;
+
+public class CalculatorTest {
+	public static void main(String args[]) {
+		Calc calc = new CompleteCalc();
+		calc.description();
+		
+		int[] arr = {1, 2, 3, 4, 5};
+		System.out.println(Calc.total(arr));
+		
+		
+	}
+}
