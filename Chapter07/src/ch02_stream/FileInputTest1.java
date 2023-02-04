d35d429b2b77a44a91dbd11bc3e046c41b946236
gabang2 <offbeat1020@naver.com>
2023-02-05 06:41:24 +0900

    FileInputStream 예제(byte단위로 출력해보기)

--- Chapter07/src/ch02_stream/FileInputTest1.java
+++ Chapter07/src/ch02_stream/FileInputTest1.java
@@ -4,22 +4,21 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+
 public class FileInputTest1 {
 	public static void main(String[] args) {
 		
-		try (FileInputStream fis = new FileInputStream("input2.txt")){
-			
+		try (FileInputStream fis =  new FileInputStream("input.txt")){
 			int i;
-			byte[] bs = new byte[10];
-			while((i = fis.read(bs)) != -1) {
-				for(byte b : bs) {
-					System.out.print((char)b);
-				}
-				System.out.println();
+			while((i = fis.read()) != -1) {
+				System.out.print((char)i);
 			}
 			
 		} catch (IOException e) {
-			e.printStackTrace(); // 어디서 에러가 났는지 확인할 수 있음
+			e.printStackTrace();
 		}
+		
+		System.out.println("\nend");
+		
 	}
 }
