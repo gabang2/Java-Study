e9be4fb47b4dc44f4d9a0099661f84adc76b8666
gabang2 <offbeat1020@naver.com>
2023-02-05 06:51:35 +0900

    FileInpuStream을 List형태로 받기

--- Chapter07/src/ch02_stream/FileInputTest1.java
+++ Chapter07/src/ch02_stream/FileInputTest1.java
@@ -8,17 +8,20 @@ import java.io.IOException;
 public class FileInputTest1 {
 	public static void main(String[] args) {
 		
-		try (FileInputStream fis =  new FileInputStream("input.txt")){
-			int i;
-			while((i = fis.read()) != -1) {
-				System.out.print((char)i);
-			}
+		try (FileInputStream fis =  new FileInputStream("input2.txt")){
 			
+			// i = fis.read(bs)에서 i에 저장되는 개수를 출력, 괄호 안에 어디다가 임시로 저장할건지 지정
+			int i;
+			byte[] bs = new byte[10];
+			while((i = fis.read(bs)) != -1) {
+				for (int k=0; k<i; k++) {
+					System.out.print((char)bs[k]);
+				}
+				System.out.println();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		System.out.println("\nend");
 		
 	}
 }
