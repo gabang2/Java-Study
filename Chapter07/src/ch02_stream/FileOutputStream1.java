fabfd6a7a18b0c13e9f154cf743bc36dffbec8f6
gabang2 <offbeat1020@naver.com>
2023-02-05 07:01:33 +0900

    FileOutputStream으로 txt파일 작성하고, FileInputStream으로 파일 읽기

--- Chapter07/src/ch02_stream/FileOutputStream1.java
+++ Chapter07/src/ch02_stream/FileOutputStream1.java
@@ -1,17 +1,28 @@
 package ch02_stream;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FileOutputStream1 {
 
+	// OutputStream으로 쓰고, InputStream으로 읽는 프로그램 작성
+	
 	public static void main(String[] args) {
-		try (FileOutputStream fos = new FileOutputStream("outputStream.txt", true)){
+		
+		byte[] bs = new byte[26];
+		byte data = 65;
+		for(int i=0; i<bs.length; i++) {
+			bs[i] = data;
+			data++;
+		}
+		try (FileOutputStream fos = new FileOutputStream("alpha.txt", true); FileInputStream fis = new FileInputStream("alpha.txt")){
 			
-			fos.write(65);
-			fos.write(66);
-			fos.write(67);
-			
+			fos.write(bs);
+			int ch;
+			while((ch = fis.read()) != -1) {
+				System.out.print((char)ch);
+			}
 		} catch (IOException e) {
 			System.out.println(e);
 		}
